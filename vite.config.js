7a66cdbaad5cb9a3f023036b48ca914c21b8820e
LENOVO\wanghf6 <wanghf6@lenovo.com>
2026-03-31 15:10:56 +0800

    feat: update vite config and api setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',  // 确保这里是 '/'，如果是 './' 可能会导致静态资源加载 404
+  base: '/SCIFTEND/',
   server: {
     proxy: {
       // 本地开发时的跨域代理（仅限 npm run dev 时有效）
